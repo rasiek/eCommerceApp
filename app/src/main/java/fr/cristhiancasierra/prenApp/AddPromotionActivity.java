eb7caae7efa9e9aed77cb63cc2facd46e943b143
Cristhian Casierra <ccasierra88@hotmail.com>
2021-11-26 20:03:47 +0100

    initial commit

---
+++ app/src/main/java/fr/cristhiancasierra/prenApp/AddPromotionActivity.java
@@ -0,0 +1,14 @@
+package fr.cristhiancasierra.prenApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPromotionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_promotion);
+    }
+}
