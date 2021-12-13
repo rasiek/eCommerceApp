daf7b65dc8f22b585d8f8a1b675e69782f03c0e1
Cristhian Casierra <ccasierra88@hotmail.com>
2021-12-13 20:38:46 +0100

    Activity connections added

---
+++ app/src/main/java/fr/cristhiancasierra/prenApp/AccountGestionProActivity.java
@@ -0,0 +1,14 @@
+package fr.cristhiancasierra.prenApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountGestionPro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_gestion_pro);
+    }
+}
