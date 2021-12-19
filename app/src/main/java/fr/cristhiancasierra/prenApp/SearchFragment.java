07fbee07b1c109e8efe3db3b305ed5228bb936ed
Cristhian Casierra <ccasierra88@hotmail.com>
2021-12-19 19:17:02 +0100

    nav modified, fragments updated

--- app/src/main/java/fr/cristhiancasierra/prenApp/SearchFragment.java
+++ app/src/main/java/fr/cristhiancasierra/prenApp/SearchFragment.java
@@ -1,19 +1,34 @@
 package fr.cristhiancasierra.prenApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
+
+    ArrayAdapter<String> arrayAdapter;
+    TextView advancedSearch;
 
 
 
@@ -26,14 +41,57 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        setHasOptionsMenu(true);
+
+        advancedSearch = view.findViewById(R.id.advanced_search);
+        advancedSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), AdvancedSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView listView = view.findViewById(R.id.my_list);
+        List<String> myliste = new ArrayList<>();
+        myliste.add("Télephone");
+        myliste.add("ordinateur");
+
+        arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, myliste);
+
+        listView.setAdapter(arrayAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.my_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search Here!");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
     }
 }
